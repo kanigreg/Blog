c3115e6d666a858245d2bd4352f71aff44c38d17
Gregory <netsafe.g@gmail.com>
2019-11-15 19:34:12 +0500

    Added following.

--- application/routes.py
+++ application/routes.py
@@ -87,5 +87,38 @@ def edit_profile():
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You can not follow yoursalf!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}!'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You can not unfollow yoursalf!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('Yot are unfollow {}.'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+
 if __name__ == '__main__':
     app.run()
